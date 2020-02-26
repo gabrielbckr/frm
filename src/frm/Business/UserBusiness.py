dda97dcb5125fc9fd68be8c5c329861a2070fae6
Gabriel Becker <gabriel.becker.59@gmail.com>
2020-02-26 20:46:35 -0300

    initializing business classes

--- src/frm/Business/UserBusiness.py
+++ src/frm/Business/UserBusiness.py
@@ -1,12 +1,16 @@
 from ..Repository.UserRepository import UserRepository
 
+
 userRepository = UserRepository()
+
 
 def add_user():
     pass
 
+
 def update_user_settings():
     pass
+
 
 def update_user_details():
     pass
